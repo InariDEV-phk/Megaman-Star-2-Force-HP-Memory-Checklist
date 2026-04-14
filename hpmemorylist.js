83029b1aa23dae0d4b8ba9bc3bf2008d1e2d4f73
InariDEV-phk <alphaphk8000@gmail.com>
2026-04-14 14:20:59 -0300

    First Commit

---
+++ hpmemorylist.js
@@ -0,0 +1,74 @@
+const hpMemList = [
+    {map:"Echo Ridge",world:"Real World",location:"Snaila Maila's HELP",item:"HP Memory 20"},
+    {map:"Echo Ridge", world:"Wave World", location:"Geo's Room ER BMW", item: "HP Memory 10"},
+    {map:"Echo Ridge", world:"Wave World", location:"Step ES BMW (Zack's Room)", item: "HP Memory 10"},
+    {map:"Echo Ridge", world:"Wave World", location:"Mailbox ES BMW", item: "HP Memory 10"},
+    {map:"Echo Ridge", world:"Wave World", location:"Junk Box ES BMW (Bud's Room)", item: "HP Memory 10"},
+    {map:"Echo Ridge", world:"Wave World", location:"BIG WAVE ER BMW", item: "HP Memory 20"},
+    {map:"Echo Ridge", world:"(SHOP)Real World", location:"BIG WAVE(3000ƶ)", item: "HP Memory 20"},
+    {map:"Echo Ridge", world:"(SHOP)Real World", location:"BIG WAVE(5000ƶ)", item: "HP Memory 20"},
+    {map:"Echo Ridge", world:"(SHOP)Real World", location:"BIG WAVE(8000ƶ)", item: "HP Memory 20"},
+
+    {map:"Wilshire Hills", world:"Real World", location:"Talk to man in Shopping Plaza, requires 50 LP", item: "HP Memory 10"},
+    {map:"Wilshire Hills", world:"Real World", location:"Styg Dismal's HELP", item: "HP Memory 20"},
+    {map:"Wilshire Hills", world:"Real World", location:"Kate Chizer's HELP", item: "HP Memory 10"},
+    {map:"Wilshire Hills", world:"Real World", location:"Carma Iffy's HELP", item: "HP Memory 10"},
+    {map:"Wilshire Hills", world:"Real World", location:"Iver Gatte's HELP", item: "HP Memory 10"},
+    {map:"Wilshire Hills", world:"Real World", location:"Aaron Boreal's HELP", item: "HP Memory 20"},
+    {map:"Wilshire Hills", world:"Wave World", location:"Shopping Plaza ER BMW", item: "HP Memory 10"},
+    {map:"Wilshire Hills", world:"Wave World", location:"IFL Tower 1 ER BMW", item: "HP Memory 10"},
+    {map:"Wilshire Hills", world:"(SHOP)Wave World", location:"Shopping Plaza ER(1000ƶ)", item: "HP Memory 20"},
+    {map:"Wilshire Hills", world:"(SHOP)Wave World", location:"Shopping Plaza ER(3000ƶ)", item: "HP Memory 20"},
+    {map:"Wilshire Hills", world:"(SHOP)Wave World", location:"Shopping Plaza ER(6000ƶ)", item: "HP Memory 20"},
+
+    {map:"Grizzly Peak", world:"Real World", location:"Examine drying skis on top floor in Peak Hotel", item: "HP Memory 10"},
+    {map:"Grizzly Peak", world:"Real World", location:"Examine Matter Wave beds in Suite", item: "HP Memory 10"},
+    {map:"Grizzly Peak", world:"Real World", location:"Talk to souvenir shop, requires 100 LP", item: "HP Memory 10"},
+    {map:"Grizzly Peak", world:"Real World", location:"Leroy Mann's HELP", item: "HP Memory 10"},
+    {map:"Grizzly Peak", world:"Real World", location:"Yugo Astray's HELP", item: "HP Memory 10"},
+    {map:"Grizzly Peak", world:"Real World", location:"Meala DeVour's HELP", item: "HP Memory 10"},
+    {map:"Grizzly Peak", world:"Real World", location:"Nival Peaks's HELP", item: "HP Memory 10"},
+    {map:"Grizzly Peak", world:"Wave World", location:"Foodtopia ER BMW", item: "HP Memory 20"},
+    {map:"Grizzly Peak", world:"Wave World", location:"Lost Ice Statue ES BMW (Foodtopia ER)", item: "HP Memory 20"},
+    {map:"Grizzly Peak", world:"Wave World", location:"Black Diamond Run ER BMW", item: "HP Memory 20"},
+
+    {map:"Mess Village", world:"Real World", location:"Examine sheep couple in Messie's Cove", item: "HP Memory 10"},
+    {map:"Mess Village", world:"Real World", location:"Examine binoculars on Observation Deck	", item: "HP Memory 10"},
+    {map:"Mess Village", world:"Real World", location:"Luz Stufalot's HELP", item: "HP Memory 10"},
+    {map:"Mess Village", world:"Real World", location:"Minor Angler's HELP", item: "HP Memory 10"},
+    {map:"Mess Village", world:"Wave World", location:"Mess Village ER BMW", item: "HP Memory 20"},
+    {map:"Mess Village", world:"Wave World", location:"Sherperd ES BMW (Messie's Cove ER)", item: "HP Memory 20"},
+    {map:"Mess Village", world:"Wave World", location:"Octy ES PMW (Loch Mess 2 ER)", item: "HP Memory 20"},
+
+    {map:"Whazzap", world:"Real World", location:"Examine right drum from roast in Whazzap", item: "HP Memory 10"},
+    {map:"Whazzap", world:"Real World", location:"Examine the Hertz mural description", item: "HP Memory 10"},
+    {map:"Whazzap", world:"Real World", location:"Wazafowonwon's HELP", item: "HP Memory 20"},
+    {map:"Whazzap", world:"Real World", location:"Wherzat's HELP", item: "HP Memory 20"},
+    {map:"Whazzap", world:"Real World", location:"Lennycia's HELP", item: "HP Memory 10"},
+    {map:"Whazzap", world:"Wave World", location:"Ammonite ES BMW (Whazzap Lines ER)", item: "HP Memory 10"},
+    {map:"Whazzap", world:"Wave World", location:"Old Dragonfly ES BMW (Whazzap Lines ER)", item: "HP Memory 10"},
+    {map:"Whazzap", world:"Wave World", location:"Whazzap Ruins 2 ER BMW", item: "HP Memory 20"},
+    {map:"Whazzap", world:"(SHOP)Wave World", location:"Whazzap Lines ER(5000ƶ)", item: "HP Memory 20"},
+    {map:"Whazzap", world:"(SHOP)Wave World", location:"Whazzap Lines ER(9000ƶ)", item: "HP Memory 20"},
+    {map:"Whazzap", world:"(SHOP)Wave World", location:"Whazzap Lines ER(14000ƶ)", item: "HP Memory 20"},
+
+    {map:"Sky Wave", world:"Wave World", location:"Mess Village SW BMW", item: "HP Memory 20"},
+    {map:"Sky Wave", world:"(SHOP)Wave World", location:"Bermuda Maze", item: "HP Memory 20"},
+    {map:"Sky Wave", world:"(SHOP)Wave World", location:"Bermuda Maze", item: "HP Memory 20"},
+    {map:"Sky Wave", world:"(SHOP)Wave World", location:"Bermuda Maze", item: "HP Memory 20"},
+
+    {map:"Continent of Mu", world:"Wave World", location:"Murian ES BMW (Room of Heroes)", item: "HP Memory 10"},
+    {map:"Continent of Mu", world:"Wave World", location:"Room of Heroes BMW", item: "HP Memory 20"},
+    {map:"Continent of Mu", world:"Wave World", location:"Examine Ancient Star Carrier in Room of Genesis	", item: "HP Memory 10"},
+
+    {map:"Alternate Dimension", world:"Wave World", location:"Broken Bus Stop ES (Alt. Echo Ridge ER)", item: "HP Memory 10"},
+    {map:"Alternate Dimension", world:"Wave World", location:"Stuck Windmill ES (Alt. Messie's Cove ER)", item: "HP Memory 20"},
+    {map:"Alternate Dimension", world:"(SHOP)Wave World", location:"Trans Dimension 1", item: "HP Memory 20"},
+    {map:"Alternate Dimension", world:"(SHOP)Wave World", location:"Trans Dimension 1", item: "HP Memory 20"},
+    {map:"Alternate Dimension", world:"(SHOP)Wave World", location:"Trans Dimension 1", item: "HP Memory 20"},
+
+    
+
+
+
+]
